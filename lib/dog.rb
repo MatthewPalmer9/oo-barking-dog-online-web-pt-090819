4d6c44bfaa3c13257e544bc718a73814dfc38ef1
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-24 22:01:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog 
+  
+  def bark
+    puts "woof!"
+  end 
+  
+  def name=(new_dogs_name)
+    @dog_name = new_dogs_name
+  end 
+  
+  def name 
+    @dog_name 
+  end 
+
+end
